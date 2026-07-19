6eae77a7608a4f61469c7511df9f6e5a53019376
Piyush Saxena <piyushsaxena168@gmail.com>
2026-07-19 13:09:26 +0530

    Update OrderService.java

--- Circular Dependency in Spring/Bean Scope/OrderService.java
+++ Circular Dependency in Spring/Bean Scope/OrderService.java
@@ -16,3 +16,27 @@ public class AppConfig {
         return new OrderService();
     }
 }
+
+/*
+suppose if scope is like @Scope("singleton")
+
+in AppConfig
+we have
+ @Bean
+    public OrderService getOrder(){
+        return new OrderService();
+    }
+    @Bean
+    public OrderService getOrder2(){
+        return new OrderService();
+    }
+
+    they both are a singleton,
+    but two different beans will be created
+    for these two bean definitions
+    it will print OrderService created two times...
+
+    singleton does not means that only one bean will exist
+    it means one bean per defintion will exist.
+
+ */
