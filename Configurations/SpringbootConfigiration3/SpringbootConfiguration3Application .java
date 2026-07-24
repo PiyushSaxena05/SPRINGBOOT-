124c62f28ddcf0eab8f9b60b787cdc9870b66547
Piyush Saxena <piyushsaxena168@gmail.com>
2026-07-24 08:27:35 +0530

    Add Spring Boot application class

---
+++ Configurations/SpringbootConfigiration3/SpringbootConfiguration3Application .java
@@ -0,0 +1,13 @@
+package com.example.SpringbootConfiguration3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConfiguration3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConfiguration3Application.class, args);
+	}
+
+}
