6c55b2cfc82b27118e2430c7ef425ed94d90fdf6
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-02 14:24:40 +0530

    Remove run code tips from main.java
    Removed unnecessary tips for running code.

--- SpringMVC/main.java
+++ SpringMVC/main.java
@@ -13,8 +13,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 
 import java.io.File;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) throws LifecycleException {
 
