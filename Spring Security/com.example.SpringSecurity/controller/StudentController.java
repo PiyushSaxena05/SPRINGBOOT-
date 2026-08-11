5e76bdbad75e11da9ad56688f24be60d64bd0686
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-11 17:13:21 +0530

    Create StudentController.java

---
+++ Spring Security/com.example.SpringSecurity/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.SpringSecurity.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @GetMapping
+    public ResponseEntity<String>getStudents(){
+        return ResponseEntity.ok("Students retrieved successfully");
+    }
+}
