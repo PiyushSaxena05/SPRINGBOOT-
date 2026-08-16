1c0221dc553de237153986b2727881f1a3e3edd1
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-16 13:40:08 +0530

    Refactor User entity by removing fields
    Removed username, password, and enabled fields from User entity.

---
+++ SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/entity/User.java
@@ -0,0 +1,14 @@
+package com.SpringSecurityDatabase.SpringSecurity.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+ 
+    
+}
