61972319c463cda144538bed79d7b3c01315772c
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-21 16:38:41 +0530

    Add Lombok annotations to UserRegisterRequestDto

--- SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/Dto/UserRegisterRequestDto.java
+++ SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/Dto/UserRegisterRequestDto.java
@@ -1,8 +1,13 @@
 package com.SpringSecurityDatabase.SpringSecurity.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserRegisterRequestDto {
     private String username;
-
     private String password;
+
 
 }
