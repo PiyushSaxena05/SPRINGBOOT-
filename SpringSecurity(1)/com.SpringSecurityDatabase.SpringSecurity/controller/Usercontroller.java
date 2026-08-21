5b8662f8b28afe31bb20c1a69c42c39c0740292f
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-21 16:38:09 +0530

    Update Usercontroller.java

--- SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/controller/Usercontroller.java
+++ SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/controller/Usercontroller.java
@@ -1,15 +1,50 @@
 package com.SpringSecurityDatabase.SpringSecurity.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.SpringSecurityDatabase.SpringSecurity.Dto.UserRegisterRequestDto;
+import com.SpringSecurityDatabase.SpringSecurity.Dto.UserRegisterResponseDto;
+import com.SpringSecurityDatabase.SpringSecurity.Service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 public class Usercontroller {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello";
+    private AuthService authService;
+
+    public Usercontroller(AuthService authService) {
+        this.authService = authService;
     }
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello";
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserRegisterResponseDto> register(
+            @RequestBody UserRegisterRequestDto registerRequestDto) {
+        UserRegisterResponseDto userRegisterResponseDto
+                = authService.register(registerRequestDto);
+
+        return ResponseEntity.ok(userRegisterResponseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Boolean> login(
+            @RequestBody UserRegisterRequestDto registerRequestDto) {
+        Boolean loggedIn = authService.login(registerRequestDto);
+
+        return ResponseEntity.ok(loggedIn);
+    }
+
+    @GetMapping("/token")
+    public CsrfToken getToken(CsrfToken csrfToken) {
+        return csrfToken;
+    }
+
 }
