a5fcaeb6b2a29756157bea74cbfbbb36324f7707
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-21 16:39:09 +0530

    Add UserRegisterResponseDto class for user registration

---
+++ SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/Dto/UserRegisterResponseDto .java
@@ -0,0 +1,12 @@
+package com.SpringSecurityDatabase.SpringSecurity.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRegisterResponseDto {
+
+    private String username;
+    private String message;
+}
