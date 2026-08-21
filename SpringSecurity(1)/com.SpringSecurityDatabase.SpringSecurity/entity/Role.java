5473c028cf1dc790387a1016bcdd72d5c4f77c9c
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-21 14:12:56 +0530

    Remove roles set and ManyToMany relationship
    Removed ManyToMany relationship and roles set from Role entity.

--- SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/entity/Role.java
+++ SpringSecurity(1)/com.SpringSecurityDatabase.SpringSecurity/entity/Role.java
@@ -21,12 +21,6 @@ public class Role {
     @Column(nullable = false,unique = true)
     private String name;
 
-    @ManyToMany
-    @JoinTable(
-            name = "user_roles",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles = new HashSet<>();
+
 
 }
